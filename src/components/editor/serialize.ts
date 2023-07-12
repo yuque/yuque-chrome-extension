a7a5e214199fe59c0e333466f0352de9738d9084
vagusx <vagusxl@gmail.com>
2023-07-12 16:45:29 +0800

    feat: handle h1..h6 serialization to lake format

--- src/components/editor/serialize.ts
+++ src/components/editor/serialize.ts
@@ -49,6 +49,18 @@ const serialize = (
         } else {
           return `<img src="${escapeHtml(node.url)}" />`;
         }
+      case 'heading-one':
+        return `<h1>${children}</h1>`;
+      case 'heading-two':
+        return `<h2>${children}</h2>`;
+      case 'heading-three':
+        return `<h3>${children}</h3>`;
+      case 'heading-four':
+        return `<h4>${children}</h4>`;
+      case 'heading-five':
+        return `<h5>${children}</h5>`;
+      case 'heading-six':
+        return `<h6>${children}</h6>`;
       default:
         return children;
     }
