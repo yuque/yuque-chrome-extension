e9fb8a57ffb40b897c605cff8ee4e60d8c3be5c9
vagusx <vagusxl@gmail.com>
2023-07-28 18:28:07 +0800

    fix: 修复确认选取按钮点击失效

--- src/pages/inject/content-scripts.ts
+++ src/pages/inject/content-scripts.ts
@@ -79,6 +79,7 @@ class App {
         verticalAlign: 'middle',
         whiteSpace: 'nowrap',
         textDecoration: 'none',
+        pointerEvents: 'all',
         transition: 'all 0.2s cubic-bezier(0.645, 0.045, 0.355, 1)',
       })
       .appendTo('.tip-mask')
