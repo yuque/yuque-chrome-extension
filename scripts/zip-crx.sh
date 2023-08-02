7a77ca01ed7b6ddf20197f8943b972a86bbc086e
vagusx <vagusxl@gmail.com>
2023-08-02 19:45:24 +0800

    build: 将 updates.xml 也打包到 crx 的 zip 中

--- scripts/zip-crx.sh
+++ scripts/zip-crx.sh
@@ -1,4 +1,4 @@
 CRUEENT_VERSION=`node -e "console.log(require('./package').version)"`
 
 # .crx 默认下载会被拦截，需要压缩下载后再解压
-zip -r $CRUEENT_VERSION.crx.zip $CRUEENT_VERSION.crx
+zip -r $CRUEENT_VERSION.crx.zip updates.xml $CRUEENT_VERSION.crx
