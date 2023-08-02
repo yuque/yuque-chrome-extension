ecd2eacdbead70f2259a9e3d2d19b3f61433c81f
达峰的夏天 <xudafeng@126.com>
2023-08-02 15:50:07 +0800

    chore: 更新构建脚本 (#68)
    * chore: 更新构建脚本
    
    * chore: 优化脚本

---
+++ scripts/common.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const path = require('path');
+const pkg = require('../package.json');
+
+const distFolder = path.resolve(__dirname, '..', 'dist', pkg.version);
+const cdnPrefix = 'https://app.nlark.com/yuque-chrome-extension';
+const sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
+
+exports.cdnPrefix = cdnPrefix;
+exports.distFolder = distFolder;
+exports.sleep = sleep;
