eaca19925103228e298bf9e5cf5cd16f38926eaf
xudafeng <xudafeng@126.com>
2023-08-03 19:51:04 +0800

    chore: 优化构建工程

--- scripts/preset-editor.js
+++ scripts/preset-editor.js
@@ -1,27 +1,34 @@
 'use strict';
 
 const path = require('path');
+const chalk = require('chalk');
 const urllib = require('urllib');
-const { promises: fs, mkdirSync, createWriteStream, createReadStream } = require('fs');
+const {
+  promises: fs,
+  mkdirSync,
+  copyFileSync,
+  createWriteStream,
+} = require('fs');
 
 const { distFolder, sleep } = require('./common');
 
 const LAKE_EDITOR_VERSION = '1.0.1-dev4';
 
-const AssetsURL = {
+const remoteAssetsUrls = {
   lakejs: `https://gw.alipayobjects.com/render/p/yuyan_npm/@alipay_lakex-doc/${LAKE_EDITOR_VERSION}/umd/doc.umd.js`,
   lakecss: `https://gw.alipayobjects.com/render/p/yuyan_npm/@alipay_lakex-doc/${LAKE_EDITOR_VERSION}/umd/doc.css`,
   codemirror: 'https://gw.alipayobjects.com/render/p/yuyan_v/180020010000005484/7.1.4/CodeMirror.js',
-  katex: require.resolve('katex/dist/katex.min.js'),
   tracert_a385: 'https://ur.alipay.com/tracert_a385.js',
 };
 
+const localAssetsPaths = {
+  katex: require.resolve('katex/dist/katex.min.js'),
+};
+
 async function downloadFile(remoteURL, localFilename) {
-  console.log(`# downaload file to ${localFilename} from ${remoteURL}`);
-  mkdirSync(path.dirname(localFilename), { recursive: true });
-  await sleep(500);
+  console.log(`# downaload file to ${chalk.cyan(localFilename)} from ${remoteURL}`);
   const fd = await fs.open(localFilename, 'w', 0o644);
-  await sleep(500);
+  await sleep(100);
   await urllib.request(remoteURL, {
     timeout: 10 * 1000,
     writeStream: createWriteStream(localFilename, {
@@ -30,36 +37,32 @@ async function downloadFile(remoteURL, localFilename) {
   });
 }
 
-async function moveFile(src, dist) {
-  console.log(`# move file to ${dist} from ${src}`);
-  mkdirSync(path.dirname(dist), { recursive: true });
-  return new Promise((resolve, reject) => {
-    createReadStream(src)
-      .pipe(createWriteStream(dist))
-      .on('end', () => {
-        resolve(true);
-      })
-      .on('error', reject);
-  });
-}
-
-const urls = Object.values(AssetsURL);
-
-module.exports.webpackCleanIgnorePatterns = urls
+module.exports.webpackCleanIgnorePatterns = Object.values({
+  ...remoteAssetsUrls,
+  ...localAssetsPaths,
+})
   .map(url => url.split('/').pop())
   .map(fileName => `!${fileName}`);
 
 module.exports.presetEditor = async function main() {
   console.log('start preset editor ...');
 
+  const urls = Object.values(remoteAssetsUrls);
   for (let i = 0; i < urls.length; i++) {
     const url = urls[i];
     const fileName = url.split('/').pop();
     const localFilename = path.resolve(distFolder, fileName);
-    if (!url.startsWith('http')) {
-      moveFile(url, localFilename);
-    } else {
-      await downloadFile(url, localFilename);
-    }
+    mkdirSync(distFolder, { recursive: true });
+    await downloadFile(url, localFilename);
+  }
+
+  const localAssets = Object.values(localAssetsPaths);
+  for (let i = 0; i < localAssets.length; i++) {
+    const filePath = localAssets[i];
+    const fileName = path.basename(filePath);
+    const localFilename = path.resolve(distFolder, fileName);
+    mkdirSync(distFolder, { recursive: true });
+    console.log(`# copy file to ${chalk.cyan(localFilename)} from ${filePath}`);
+    copyFileSync(filePath, localFilename);
   }
 };
