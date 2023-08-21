40f4e3ce54948ba4967202ad3cd6fdc256f5f6d3
ilimei <514222057@qq.com>
2023-08-21 16:15:08 +0800

    fix: use js url instead inline script

--- src/components/lake-editor/kernel-editor.tsx
+++ src/components/lake-editor/kernel-editor.tsx
@@ -11,7 +11,7 @@ export interface KernelEditorProps {
   onLoad?: () => void;
 }
 
-const key = 'ID' + parseInt(Math.random() * Math.pow(10, 10) + '', 10).toString(16);
+const key = 'kernel-editor';
 const getRequestID = (() => {
   let id = 0;
   return () => {
@@ -63,129 +63,7 @@ const templateHtml = `
 </head>
 <body>
   <script src="chrome-extension://ehnlicbeaedobffiadggeebjbmcgjnjh/doc.umd.js"></script>
-  <script>
-    const { KernelFactory, Plugins } = window.Doc;
-    const CoreKernelFactory = new KernelFactory();
-
-    const OpenKernelPlugins = [
-      Plugins.Alert.AlertPlugin,
-      Plugins.Meta.MetaPlugin, // 要在 layout 前
-      Plugins.Alignment.AlignmentPlugin,
-      Plugins.BasicTextStyle.BasicTextStylePlugin,
-      Plugins.BgColor.BgColorPlugin,
-      Plugins.BreakLine.BreakLinePlugin,
-      Plugins.Card.CardPlugin,
-      Plugins.ClearFormat.ClearFormatPlugin,
-      Plugins.Clipboard.ClipboardPlugin,
-      Plugins.Code.CodePlugin,
-      Plugins.Color.ColorPlugin,
-      Plugins.Container.ContainerPlugin,
-      Plugins.Content.ContentPlugin,
-      Plugins.Delete.DeletePlugin,
-      Plugins.DropFile.DropFilePlugin,
-      Plugins.FallbackCard.FallbackCardPlugin,
-      Plugins.File.FileCardPlugin,
-      Plugins.FileReader.FileReaderPlugin,
-      Plugins.Focus.FocusPlugin,
-      Plugins.Fontsize.FontsizePlugin,
-      Plugins.FormatPainter.FormatPainterPlugin,
-      Plugins.Heading.HeadingPlugin,
-      Plugins.History.HistoryPlugin,
-      Plugins.Hr.HrCardPlugin,
-      Plugins.HtmlDataSource.HTMLDataSourcePlugin,
-      Plugins.Image.ImagePlugin,
-      Plugins.Indent.IndentPlugin,
-      Plugins.InodeDataSource.INodeReaderPlugin,
-      Plugins.Input.InputPlugin,
-      Plugins.Label.LabelCardPlugin,
-      Plugins.LakeDataSource.LakePlugin,
-      Plugins.Layout.LayoutPlugin,
-      Plugins.LineHeight.LineHeightPlugin,
-      Plugins.Link.LinkPlugin,
-      Plugins.List.ListPlugin,
-      Plugins.Mark.MarkPlugin,
-      Plugins.Markdown.MarkdownPlugin,
-      Plugins.MarkdownPasteParse.MarkdownPasteParsePlugin,
-      Plugins.Paragraph.ParagraphPlugin,
-      Plugins.QuickInput.QuickInputPlugin,
-      Plugins.Quote.QuotePlugin,
-      Plugins.Save.SavePlugin,
-      Plugins.Selectall.SelectAllPlugin,
-      Plugins.Selection.SelectionPlugin,
-      Plugins.Slash.SlashPlugin,
-      Plugins.Subscript.SubscriptPlugin,
-      Plugins.Table.TablePlugin,
-      Plugins.TextDataSource.TextDataSourcePlugin,
-      Plugins.Typography.TypographyPlugin,
-      Plugins.MarkdownDataSource.MarkdownDataSourcePlugin,
-      Plugins.Codeblock.CodeBlockCardPlugin,
-      Plugins.Mention.MentionCardPlugin,
-      Plugins.Video.VideoPlugin,
-      Plugins.Columns.ColumnsPlugin,
-      Plugins.Collapse.CollapsePlugin,
-      Plugins.Math.MathCardPlugin,
-      Plugins.Thirdparty.ThirdpartyPlugin,
-      Plugins.KernelAssistant.KernelAssistantPlugin,
-      Plugins.Summary.SummaryPlugin,
-    ];
-    CoreKernelFactory.registerKernelPlugin(OpenKernelPlugins);
-
-    // 创建编辑器
-    window.newEditor = CoreKernelFactory.createKernel({});
-
-    window.addEventListener('message', function (event) {
-      if (event.data.key === '${key}') {
-        switch (event.data.type) {
-          case 'set-content':
-            window.newEditor.setDocument(event.data.data.type, event.data.data.content);
-            window.parent.postMessage({
-              key: '${key}',
-              requestId: event.data.requestId,
-              type: 'set-content-success',
-            }, '*');
-            break;
-          case 'get-content':
-            window.parent.postMessage({
-              key: '${key}',
-              requestId: event.data.requestId,
-              type: 'get-content',
-              data: window.newEditor.getDocument(event.data.data.type),
-            }, '*');
-            break;
-          case 'get-summary-content':
-            window.parent.postMessage({
-              key: '${key}',
-              requestId: event.data.requestId,
-              type: 'get-summary-content',
-              data: window.newEditor.queryCommandValue('getSummary', 'lake'),
-            }, '*');
-            break;
-          case 'word-count':
-            window.parent.postMessage({
-              key: '${key}',
-              requestId: event.data.requestId,
-              type: 'word-count',
-              data: window.newEditor.queryCommandValue('wordCount'),
-            }, '*');
-            break;
-          case 'is-empty':
-            window.parent.postMessage({
-              key: '${key}',
-              requestId: event.data.requestId,
-              type: 'is-empty',
-              data: window.newEditor.queryCommandValue('isEmpty'),
-            }, '*');
-            break;
-          default:
-            break;
-        }
-      }
-    });
-    window.parent.postMessage({
-      key: '${key}',
-      type: 'editor-ready',
-    }, '*');
-  </script>
+  <script src="chrome-extension://ehnlicbeaedobffiadggeebjbmcgjnjh/kernel-editor.js"></script>
 </body>
 </html>
 `;
@@ -210,7 +88,7 @@ export default forwardRef<IKernelEditorRef, KernelEditorProps>((props, ref) => {
       }
 
       if (event.data.type === 'editor-ready') {
-        setSendMessage(requestData => {
+        setSendMessage(() => requestData => {
           return new Promise(resolve => {
             const requestId = getRequestID();
             resolveCache.set(requestId, data => {
