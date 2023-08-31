0b4c8738cfab810fe2268ec301a85431f04828b5
淮然 <50158871+moshangqi@users.noreply.github.com>
2023-08-31 20:39:15 +0800

    feat: 增加 ocr 模型调用的基础能力 (#140)

--- src/core/link-helper.ts
+++ src/core/link-helper.ts
@@ -6,6 +6,7 @@ const LinkHelper = {
   goMyPage: (account: {login: string}) => `${YUQUE_DOMAIN}/${account.login}`,
   unInstallFeedback: 'https://www.yuque.com/forms/share/c454d5b2-8b2f-4a73-851b-0f3d2ae585fb?chromeExtensionUninstall=true',
   introduceExtension: 'https://www.yuque.com/yuque/yuque-browser-extension/welcome#acYWK',
+  ocrProxyPage: 'https://www.yuque.com/r/ocr_proxy_page',
 };
 
 export default LinkHelper;
