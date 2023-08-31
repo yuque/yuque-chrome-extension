bb9778b5fdbdbac1b4b5fe639e14bbf2dfc7ea34
moshangqi <2509678669@qq.com>
2023-08-31 10:48:13 +0800

    feat: 卸载链接增加特殊访问标识

--- src/core/link-helper.ts
+++ src/core/link-helper.ts
@@ -4,7 +4,7 @@ const LinkHelper = {
   goDoc: (doc: {id:string}) => `${YUQUE_DOMAIN}/go/doc/${doc.id}`,
   goMyNote: () => `${YUQUE_DOMAIN}/dashboard/notes`,
   goMyPage: (account: {login: string}) => `${YUQUE_DOMAIN}/${account.login}`,
-  unInstallFeedback: 'https://www.yuque.com/forms/share/c454d5b2-8b2f-4a73-851b-0f3d2ae585fb',
+  unInstallFeedback: 'https://www.yuque.com/forms/share/c454d5b2-8b2f-4a73-851b-0f3d2ae585fb?chromeExtensionUninstall=true',
   introduceExtension: 'https://www.yuque.com/yuque/yuque-browser-extension/welcome#acYWK',
 };
 
