30634b5992fd7f8092ac734ae980e534b4bb706a
moshangqi <2509678669@qq.com>
2023-11-10 17:42:05 +0800

    feat: 抽取一些公共组件 & 修复设置页样式问题

--- src/components/SuperSideBar/render/MessageProgress/index.tsx
+++ src/components/SuperSideBar/render/MessageProgress/index.tsx
@@ -1,9 +1,6 @@
 import React from 'react';
 import classnames from 'classnames';
-import { Spin } from 'antd';
 import { IMessageProgress, IRenderProgressOptions } from '../../declare';
-import { CONVERSATION_STEP_TYPE_STATUS } from '@/isomorphic/constant/ai';
-import LarkIcon from '@/components/LarkIcon';
 import Typography from '@/components/Typography';
 import styles from './index.module.less';
 
@@ -21,38 +18,9 @@ export const MessageProgress = (props: Props) => {
       style={style}
     >
       {progresses.map((item: IMessageProgress, index: number) => {
-        const { status, icon, text } = item;
+        const { icon, text } = item;
 
         let realIcon = icon;
-        if (!realIcon) {
-          if (status === CONVERSATION_STEP_TYPE_STATUS.SUCCESS) {
-            realIcon = (
-              <LarkIcon
-                className={styles.icon}
-                size={16}
-                name={'check' as any}
-              />
-            );
-          } else if (status === CONVERSATION_STEP_TYPE_STATUS.FAILED) {
-            realIcon = (
-              <LarkIcon
-                className={styles.icon}
-                size={16}
-                name={'exclamationcircle' as any}
-              />
-            );
-          } else if (status === CONVERSATION_STEP_TYPE_STATUS.PENDING) {
-            realIcon = (
-              <Spin
-                size="small"
-                indicator={
-                  <LarkIcon size={16} name="icon-loading-green" spin />
-                }
-                className={styles.icon}
-              />
-            );
-          }
-        }
 
         return (
           <div className={styles.item} key={index}>
