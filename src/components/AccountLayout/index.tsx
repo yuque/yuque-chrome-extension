130c48c116ca777e6ce571eb091929c9f53c61e7
moshangqi <2509678669@qq.com>
2023-12-27 17:36:20 +0800

    feat: 移除第三方 cookie 检测

--- src/components/AccountLayout/index.tsx
+++ src/components/AccountLayout/index.tsx
@@ -1,13 +1,9 @@
 import React, { useState, useCallback, useMemo, useEffect } from 'react';
-import { Modal } from 'antd';
 import { IUser } from '@/isomorphic/interface';
-import { __i18n } from '@/isomorphic/i18n';
 import { pageEvent } from '@/core/event/pageEvent';
 import { PageEventTypes } from '@/isomorphic/event/pageEvent';
 import { storage } from '@/isomorphic/storage';
 import { useEffectAsync } from '@/hooks/useAsyncEffect';
-import { backgroundBridge } from '@/core/bridge/background';
-import { findCookieSettingPage } from '@/core/uitl';
 import { STORAGE_KEYS } from '@/config';
 import Login from './Login';
 import { AccountContext } from './context';
@@ -43,28 +39,6 @@ function AccountLayout(props: IAccountLayoutProps) {
       return;
     }
     try {
-      if (!navigator.cookieEnabled) {
-        new Promise(resolve => {
-          const pageUrl = findCookieSettingPage();
-          Modal.info({
-            content: __i18n('请前往「隐私和安全」打开「允许第三方cookies」，避免登录失败'),
-            title: __i18n('使用提示'),
-            closable: true,
-            icon: null,
-            okText: pageUrl ? __i18n('打开隐私和安全') : __i18n('确定'),
-            autoFocusButton: null,
-            onOk: () => {
-              if (pageUrl) {
-                backgroundBridge.tab.create(pageUrl);
-              }
-              resolve(true);
-            },
-            afterClose: () => {
-              resolve(true);
-            },
-          });
-        });
-      }
       setUser(info);
     } catch (error) {
       console.log('init user error:', error);
